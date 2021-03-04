6930bc1a0b56f934e1e64735001fc6243602cc54
Aske Bækdal Møller <askebm@live.dk>
2021-03-04 14:41:26 +0100

    Support out of root files

--- mmdc.sh
+++ mmdc.sh
@@ -1,10 +1,62 @@
 #!/bin/bash
-INPUT="${@}"
 CONTAINER="minlag/mermaid-cli"
+shopt -s extglob
+declare -a EXCEMPT_SWITCHES=('-o' '--output')
+EXCEMPT_FLAG=0
+COMMAND="mmdc"
+VOLUMES=""
 
-INPUT2=$(echo "${INPUT}" | sed -E 's+(([^-]|)-[iocCp] ?)(\S\+)+\1'"${PWD}/"'\3+g')
-INPUT3=$(echo "${INPUT2}" | sed -E \
-	's+( ?--(input|output|configFile|cssFile|puppeteerConfigFile) ?)(\S\+)+\1'"${PWD}/"'\2+g')
+update_command() {
+	local ITEM=$1
+	if [[ -e $ITEM ]] || [[ $EXCEMPT_FLAG -eq 1 ]] ; then
+		if [[ -z $(echo "$ITEM" | grep -Eo "^/") ]]; then
+			ITEM="${PWD}/${ITEM}"
+		fi
+		local VOL=$(dirname ${ITEM})
+		VOLUMES="${VOLUMES} -v ${VOL}:${VOL}"
+	fi 
+	COMMAND="$COMMAND ${ITEM}"
+	EXCEMPT_FLAG=0
+}
 
-docker run -it --rm -v ${PWD}:${PWD} ${CONTAINER} mmdc ${INPUT3}
+is_excempt(){
+	for i in "${EXCEMPT_SWITCHES[@]}"; do
+		[[ "$i" = "$1" ]] && echo 1 && return 1
+	done
+	echo 0
+	return 0
+}
+
+while true; do
+	case "$1" in
+		'--'*)
+			EXCEMPT_FLAG=$(is_excempt "$1")
+			COMMAND="$COMMAND ${1}"
+			shift
+			;;
+		'-'@([A-Za-z]))
+			EXCEMPT_FLAG=$(is_excempt "$1")
+			COMMAND="$COMMAND ${1}"
+			shift
+			;;
+		'-'*)
+			SWITCH=$(echo "$1" | grep -Eo "^-[A-Za-z]")
+			COMMAND="$COMMAND $SWITCH"
+			EXCEMPT_FLAG=$(is_excempt "$SWITCH")
+			update_command $(echo "${1}" | sed -e 's/^-[A-Za-z]//')
+			shift
+			;;
+		!(-| |))
+			update_command "$1"
+			shift
+			;;
+		*)
+			break
+	esac
+done
+
+
+#echo "$VOLUMES"
+#echo "$COMMAND"
+docker run -it --rm ${VOLUMES} ${CONTAINER} ${COMMAND}
 
